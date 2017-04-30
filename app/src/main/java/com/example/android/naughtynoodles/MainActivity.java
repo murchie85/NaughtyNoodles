6d47f429f68351002fbde9eea933a8a1e64d5d2a
Adam <murchie85@gmail.com>
2017-04-30 23:58:32 +0100

    latest

--- app/src/main/java/com/example/android/naughtynoodles/MainActivity.java
+++ app/src/main/java/com/example/android/naughtynoodles/MainActivity.java
@@ -4,7 +4,12 @@ package com.example.android.naughtynoodles;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.os.Build.VERSION_CODES.M;
+import static com.example.android.naughtynoodles.R.drawable.noodle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +20,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    /***
+     *  image view transparancy
+     */
+
+    int noodleAmount = 1;
+
     /**
-     * This method is linked ot the activity main submit text, via the onlcick method
+     * SUBMIT BUTTON LINKED TO THIS METHOD
      */
     public void submitOrder(View view) {
-        int noodleAmount = 2;
-        display(noodleAmount);
         displayPrice(noodleAmount * 3);
+    }
+
+    /**
+     * PLUS BUTTON LINKED TO THIS METHOD
+     */
+    public void increment(View view) {
+
+        if (noodleAmount < 10) {
+            noodleAmount = noodleAmount + 1;
+            display(noodleAmount);
+        } else {
+            //display in short period of time
+            Toast.makeText(getApplicationContext(), "10 Coffees Max", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    /**
+     * minus BUTTON LINKED TO THIS METHOD
+     */
+
+    public void decrement(View view) {
+
+        if (noodleAmount > 1) {
+            noodleAmount = noodleAmount - 1;
+            display(noodleAmount);
+        } else {
+            //display in short period of time
+            Toast.makeText(getApplicationContext(), "Minimum 1 coffee", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
     }
 
     /**
