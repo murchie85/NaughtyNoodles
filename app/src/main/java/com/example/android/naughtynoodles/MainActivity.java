2aa593799b7043f7014be1d5cdec0401b143775c
Adam <murchie85@gmail.com>
2017-04-30 17:55:38 +0100

    more

--- app/src/main/java/com/example/android/naughtynoodles/MainActivity.java
+++ app/src/main/java/com/example/android/naughtynoodles/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.naughtynoodles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    /**
+     * This method is linked ot the activity main submit text, via the onlcick method
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /**
+     * This is a simple method that makes the input number the textview
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+
+
 }
+
